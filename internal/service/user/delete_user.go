541f290c98078e2e084b5aaacb5862891ea5b84f
kaiiiman <bek.nurken12@mail.ru>
2025-10-24 14:19:16 +0500

    Add-delete_user

---
+++ internal/service/user/delete_user.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/nurkenti/furnitureShop/db/sqlc"
+)
+
+func DeleteUserByEmail(q *sqlc.Queries) error {
+	email, err := addInfo("Option: Delete\nEmail: ")
+	if err != nil {
+		return err
+	}
+	user, err := q.GetUserByEmail(context.Background(), email)
+	if err != nil {
+		return err
+	}
+	FormatInfo(user)
+	err = q.DeleteUserByEmail(context.Background(), email)
+	if err != nil {
+		log.Fatal("User has not delete")
+	} else {
+		fmt.Println("User has been delete")
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/nurkenti/furnitureShop/db/sqlc"
+	service "github.com/nurkenti/furnitureShop/internal/service/user"
 	"github.com/nurkenti/furnitureShop/menu"
 )
 
+var Queries *sqlc.Queries
+
+const (
+	dbSourse = "postgresql://nurken:123nura123@127.0.0.1:5433/furnitureShop?sslmode=disable"
+)
+
 func main() {
-	Salesman()
+	conn, err := pgx.Connect(context.Background(), dbSourse)
+	if err != nil {
+		log.Fatal("sex")
+	}
+	defer conn.Close(context.Background())
+	Queries = sqlc.New(conn)
+
+	service.Authorisation(Queries)
+	//Salesman()
+
 }
 
 func Salesman() {
