e1a0677aa2d02bf3b276b14b76cf991950b8df2b
kaiiiman <bek.nurken12@mail.ru>
2025-10-24 18:28:50 +0500

    Add db products

---
+++ db/queries/product.sql
@@ -0,0 +1,46 @@
+CREATE TYPE "chair_model" AS ENUM (
+  'sonyx',
+  'kurumi'
+);
+
+CREATE TYPE "wardrobe_model" AS ENUM (
+  'unibi',
+  'facito'
+);
+
+CREATE TYPE "chair_material" AS ENUM (
+  'wood',
+  'metal',
+  'fabric'
+);
+
+CREATE TYPE "wardrobe_material" AS ENUM (
+  'mdf',
+  'dsp'
+);
+
+CREATE TABLE "chair" (
+  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid),
+  "model" chair_model NOT NULL DEFAULT (sonyx),
+  "material" chair_material DEFAULT (wood),
+  "price" float,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "wardrobe" (
+  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid),
+  "model" wardrobe_model NOT NULL DEFAULT (unibi),
+  "material" wardrobe_material DEFAULT (mdf),
+  "price" float NOT NULL,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "warehose" (
+  "chair_sonyx_instoce" int,
+  "chair_kurumi_instoce" int,
+  "wardrobe_unibi_instoce" int,
+  "wardrobe_facito_instoce" int,
+  "total_products" bigint
+);
+
+CREATE UNIQUE INDEX ON "users" ("email");
